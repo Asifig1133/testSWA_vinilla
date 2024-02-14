6e537747d104dcf5ed16bd83024cc530b64bb2ac
agoshenatti <agoshenatti@icubecsi.com>
2024-02-14 12:21:57 +0530

    commiting to test the env

--- api/function_app.py
+++ api/function_app.py
@@ -10,7 +10,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
                         command_text="select [Id], [order], [title], [url], [completed] from dbo.ToDo",
                         command_type="Text",
                         connection_string_setting="SqlConnectionString")
-
+#testing the commit
 
 def get_products(req: func.HttpRequest, products: func.SqlRowList) -> func.HttpResponse:
     rows = list(map(lambda r: json.loads(r.to_json()), products))
